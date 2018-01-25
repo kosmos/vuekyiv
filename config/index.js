1d013e16a3f16472ac87b30537be831a42e0722d
Kosmos <kosmos.ehp@gmail.com>
2018-01-25 19:03:39 +0200

    Proper paths

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vuekyiv/',
     proxyTable: {},
 
     // Various Dev Server settings
